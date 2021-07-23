9bf3e36b6d47914786a0a002c4f38242c27f69bc
Mehdi Seyedain <mahdi.seyedain@gmail.com>
2021-07-23 13:46:34 +0430

    Add files via upload

---
+++ Project01_ExpenseTracker/src/components/Expenses/ExpensesList.js
@@ -0,0 +1,23 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+function ExpensesList(props) {
+  if (props.items.length === 0) {
+    return <p className="expenses-list__fallback">Found no expenses.</p>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ExpensesList;
